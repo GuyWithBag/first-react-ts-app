01f072cf5d51deef8f64414d53ba986b1c2cffbc
LoejeeMan <72646329+GuyWithBag@users.noreply.github.com>
2023-09-11 13:45:32 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Basically the Main function
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
